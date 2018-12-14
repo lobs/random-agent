9ad0caec3f58e4adc2b94370da1c5f235808594a
Lewis <6080202+lobs@users.noreply.github.com>
2018-12-14 12:49:07 +0000

    Update UserAgent.py

--- UserAgent.py
+++ UserAgent.py
@@ -1,41 +1,41 @@
-from requests import get as GET
+from requests import get as GET, exceptions
 
 class UserAgent():
 	domain = "http://useragent.ddns.net"
 
 	def RandomAgent():
-		ua = NewUserAgent.get_data("/")
-		return NewUserAgent.format_ua(ua)
+		ua = UserAgent.__get_data("/")
+		return UserAgent.__format_ua(ua)
 
 	def FirefoxAgent():
-		ua = NewUserAgent.get_data("/firefox")
-		return NewUserAgent.format_ua(ua)
+		ua = UserAgent.__get_data("/firefox")
+		return UserAgent.__format_ua(ua)
 
 	def ChromeAgent():
-		ua = NewUserAgent.get_data("/chrome")
-		return NewUserAgent.format_ua(ua)
+		ua = UserAgent.__get_data("/chrome")
+		return UserAgent.__format_ua(ua)
 
 	def OperaAgent():
-		ua = NewUserAgent.get_data("/opera")
-		return NewUserAgent.format_ua(ua)
+		ua = UserAgent.__get_data("/opera")
+		return UserAgent.__format_ua(ua)
 
 	def SafariAgent():
-		ua = NewUserAgent.get_data("/safari")
-		return NewUserAgent.format_ua(ua)
+		ua = UserAgent.__get_data("/safari")
+		return UserAgent.__format_ua(ua)
 
 	def ChromiumAgent():
-		ua = NewUserAgent.get_data("/chromium")
-		return NewUserAgent.format_ua(ua)
+		ua = UserAgent.__get_data("/chromium")
+		return UserAgent.__format_ua(ua)
 
 	def IEAgent():
-		ua = NewUserAgent.get_data("/ie")
-		return NewUserAgent.format_ua(ua)
+		ua = UserAgent.__get_data("/ie")
+		return UserAgent.__format_ua(ua)
 
-	def get_data(path):
+	def __get_data(path):
 		try:
-			return GET("{}{}".format(NewUserAgent.domain, path)).json()
-		except:
-			raise ConnectionError("Failed to connect to {}".format(NewUserAgent.domain))
+			return GET("{}{}".format(UserAgent.domain, path)).json()
+		except exceptions.ConnectionError:
+			raise ConnectionError("Failed to connect to {}".format(UserAgent.domain))
 
-	def format_ua(x):
+	def __format_ua(x):
 		return x["user_agent"]
